5c9632e7cf7affab7a15379c61b23649d311ee6f
Arthur Correnson <arthur.correnson@gmail.com>
2020-03-02 17:22:01 +0100

    Add documentation

--- difflist.ml
+++ difflist.ml
@@ -1,37 +1,69 @@
+(** Difference List implementation for OCaml.
+
+    {1 Use cases}
+
+    List concatenation in OCaml can be a nightmare. This little modules aims to provides a replacement for the list type for use in concatenation-heavy programs. Optimization are performed using so called Difference List.
+
+    {1 Benefits}
+
+    Difflist gives a O(1) function for list concatenation. This function is lazy : the content of the list is opaque until final conversion to standard ocaml list *)
+
+(** Type for Difference List *)
 type ('a, 'b) dlist = DList of ('a list -> 'b list)
+
+(** Alias for Difference List *)
 type ('a, 'b) t = ('a, 'b) dlist
 
+(** The empty list *)
 let empty = DList(fun xs -> xs)
+
+(** Singleton. ([snoc x] is [[x]]) *)
 let snoc x = DList(fun xs -> x::xs)
+
+(** Transformed singleton. ([snoct t x] is [[t x]]). Because of Lasy evaluation, the application of [t] on [x] is performed ONLY during the final conversion (see {!to_list}) 
+    @param  f   Transformation function
+    @param  x   Element
+*)
 let snoct f x = DList(fun xs -> (f x)::xs)
+
+(** List concatenation.
+    @param  f   A Difference List
+    @param  g   A Difference List
+*)
 let append f g =
   match (f,g) with
   | (DList f, DList g) -> DList(fun xs -> f (g xs))
 
+(** Build a difference list from a list *)
 let rec of_list =
   function
   | [] -> empty
   | x::xs -> append (snoc x) (of_list xs)
 
-let rec of_list_t f =
-  function
-  | [] -> empty
-  | x::xs -> append (snoc (f x)) (of_list_t f xs)
-
+(** Build a list from a difference list. Performs every computation delayed for   lazyness *)
 let to_list =
   function DList f -> f []
 
+(** Concatenate many difference list
+    @param l  A list of difference list to concatenate *)
 let concat l =
   List.fold_left append empty l
 
+(** Alias for {!append} *)
 let (++) a b = append a b
-let (<+) d l = append d (of_list l)
-let (<@) d x = append d (snoc x)
+
+(** [d <@ l] concatenate a difference list with a list *)
+let (<@) d l = append d (of_list l)
+
+(** [d <+ x] append an element (rights) to a difference list *)
+let (<+) d x = append d (snoc x)
 
 let map f =
   function DList fl -> DList(fun xs -> List.map f (fl xs))
+
 let fold_right f l acc =
   match l with DList fl -> List.fold_right f (fl []) acc
+
 let fold_left f acc =
   function DList fl -> List.fold_left f acc (fl [])
 
